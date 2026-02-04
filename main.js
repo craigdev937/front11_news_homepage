7ea367c5af1db1d159db4bdf4d545d78e7074778
Craig Johnson <craigdev937@gmail.com>
2026-02-03 20:04:16 -0500

    Added a container query for the bottom section.

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-const menuToggle = document.querySelector('.menu-toggle');
+const menuToggle = document.querySelector('.menu__toggle');
 const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll(".nav a");
 
-menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('open');
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("open");
+    menuToggle.classList.toggle("open");
+
+    const isOpen = nav.classList.contains("open");
+    menuToggle.setAttribute("aria-expanded", isOpen);
+    document.body.style.overflow = isOpen ? "hidden" : "";
 });
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("open");
+        menuToggle.classList.remove("open");
+        menuToggle.setAttribute("aria-expanded", "false");
+        document.body.style.overflow = "";
+    })
+});
+
+nav.addEventListener("click", (event) => {
+    if (event.target === nav) {
+        nav.classList.remove("open");
+        menuToggle.classList.remove("open");
+        menuToggle.setAttribute("aria-expanded", "false");
+        document.body.style.overflow = "";
+    }
+});
+
 
 
